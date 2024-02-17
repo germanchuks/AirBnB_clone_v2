45ebbed06ae6a1628c78e19de93fd9843ffcd111
Daniel German <danielchuksgerman@gmail.com>
2024-02-17 08:25:39 -0800

    HBNB is alive!

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Script to start a Flask web application."""
+from models import *
+from models import storage
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays a HTML page like 8-index.html."""
+    states = storage.all('State').values()
+    cities = storage.all('City').values()
+    amenities = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    return render_template('100-hbnb.html', states=states,
+                           amenities=amenities, cities=cities,
+                           places=places)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """Remove the current SQLAlchemy Session after each request."""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
