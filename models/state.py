65a600e1caa1bb9c056b0b1deb0c45923788d3c9
Daniel German <danielchuksgerman@gmail.com>
2024-02-17 02:18:49 -0800

    List of states - fix

--- models/state.py
+++ models/state.py
@@ -15,7 +15,7 @@ class State(BaseModel, Base):
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         cities = relationship(
             'City',
-            cascade='all, delete, delete-orphan',
+            cascade='all, delete',
             backref='state'
         )
     else:
